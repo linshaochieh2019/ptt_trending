7bf60197062dd2241e9e1500b5d700e17ccbf467
Jay Lin <linshaochieh2019@gmail.com>
2023-04-11 11:16:12 +0800

    Using env var APP_URL

--- src/tests/test_integration.py
+++ src/tests/test_integration.py
@@ -1,18 +1,21 @@
+import os
 import unittest
 import requests
+
+app_url = os.environ.get('APP_URL')
 
 class TestIntegration(unittest.TestCase):
 
     def test_index(self):
-        response = requests.get('http://localhost:5000/')
+        response = requests.get(f'{app_url}/')
         self.assertEqual(response.status_code, 200)
 
     def test_form_submission(self):
         with requests.Session() as session:
-            response = session.get('http://localhost:5000/posts')
+            response = session.get(f'{app_url}/posts')
             self.assertEqual(response.status_code, 200)
 
             form_data = {'board': 'Gossiping', 'num_pages': 1}
-            response = session.post('http://localhost:5000/posts', data=form_data)
+            response = session.post(f'{app_url}/posts', data=form_data)
             self.assertEqual(response.status_code, 200)
             self.assertIn('<div class="post">', response.text) # check if there's at least one post content
