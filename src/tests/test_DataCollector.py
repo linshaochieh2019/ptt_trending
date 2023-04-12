0673d16d1853c1eb1534e175cd82f1605b1c3b9a
Jay Lin <linshaochieh2019@gmail.com>
2023-04-12 10:09:06 +0800

    Added test_not_saving_duplicated_posts

--- src/tests/test_DataCollector.py
+++ src/tests/test_DataCollector.py
@@ -29,6 +29,21 @@ class TestDataCollector(unittest.TestCase):
             self.assertEqual(saved_post.content, post['content'])
             self.assertEqual(saved_post.board, post['board'])
 
+    def test_not_saving_duplicated_posts(self):
+        with app.app_context():
+            post = {
+                'title': 'test title',
+                'author': 'test author',
+                'created': datetime.strptime('2022-04-10 12:34:56', '%Y-%m-%d %H:%M:%S'),
+                'content': 'test content',
+                'board': 'test_board'
+            }
+            collector = DataCollector()
+            collector.posts = [post]
+            collector.save_posts()
+            num_saved_post = Post.query.filter_by(board='test_board').count()
+            self.assertEqual(num_saved_post, 1)
+
     def tearDown(self):
         with app.app_context():
             Post.query.filter_by(board='test_board').delete()
